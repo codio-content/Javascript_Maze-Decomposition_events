70b60eeda8b16c7052814908726cda9ec25ae219
SeanPackham <sean@seanpackham.com>
2015-07-01 06:36:33 +0000

    provide empty functions

--- public/js/diy.js
+++ public/js/diy.js
@@ -1,3 +1,26 @@
 
 // Write your challenge code below
 
+function initGame() {
+  
+}
+
+function turnTaken() {
+
+}
+
+function hitWallEvent() {
+
+}
+
+function hitEnergyEvent() {
+
+}
+
+function hitMonsterEvent() {
+
+}
+
+function goalReachedEvent() {
+
+}
